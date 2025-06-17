41bbfc42b751e44877711e459a6bd60101bdd996
hsrobotics <hsrobotics@HS-Robotics-03.ad.gccsmi.org>
2025-06-17 18:10:05 -0400

    Honk command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.HonkCommand;
 import frc.robot.subsystems.Drivebase;
 
 public class RobotContainer {
@@ -25,7 +26,9 @@ public class RobotContainer {
     }, drivebase));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.leftStick().whileTrue(new HonkCommand());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
---
+++ src/main/java/frc/robot/commands/HonkCommand.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import com.ctre.phoenix6.controls.MusicTone;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class HonkCommand extends Command{
+    private final TalonFX controller1 = new TalonFX(11);
+    private final TalonFX controller2 = new TalonFX(15);
+    private final TalonFX controller3 = new TalonFX(13);
+    private final TalonFX controller4 = new TalonFX(12);
+
+    private final MusicTone honkTone = new MusicTone(440);
+    private final MusicTone stopHonk = new MusicTone(0);
+
+    public HonkCommand() {
+        super();
+    }
+
+    @Override
+    public void execute() {
+        sendTone(honkTone);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        sendTone(stopHonk);
+        System.out.print("stopped");
+    }
+
+    private void sendTone(MusicTone tone)
+    {
+        controller1.setControl(tone);
+        controller2.setControl(tone);
+        controller3.setControl(tone);
+        controller4.setControl(tone);
+    }
+}
+
